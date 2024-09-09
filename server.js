ed1e097923a876b09d357268d7eae60286fd0f31
anushkapunekar <160164659+anushkapunekar@users.noreply.github.com>
2024-09-09 11:42:13 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-
+import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(
