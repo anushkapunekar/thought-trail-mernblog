81367359697ecf351884526d4274bbd375493cda
anushkapunekar <160164659+anushkapunekar@users.noreply.github.com>
2024-09-09 11:38:02 +0530

    connected to the database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home"
 import About from "./pages/about"
 import Dashboard from "./pages/Dashboard"
 import Project from "./pages/Project"
+import Header from "./components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       < Route path = "/about" element = {<About/>}/>
