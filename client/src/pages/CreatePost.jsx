6b1ef2aaec76873b48dcbe9cb76c70bd52c1227e
anushkapunekar <160164659+anushkapunekar@users.noreply.github.com>
2024-09-23 17:52:27 +0530

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,4 @@
-import { Select, TextInput, FileInput , Button, Alert} from "flowbite-react"
+import { Select, TextInput, FileInput, Button, Alert } from "flowbite-react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
@@ -6,41 +6,42 @@ import { app } from "../firebase";
 import { useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
-    const [file ,setFile]=useState(null);
-    const [imageUploadProgress , setImageUploadProgress] = useState(null);
-    const [imageUploadError , setImageUploadError]=
-    useState(null);
-    const [formData , setFormData]= useState({});
+    const [file, setFile] = useState(null);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
 
-    const handleUploadImage = async ()=>{
+    const handleUploadImage = async () => {
         try {
-            if(!file){
+            if (!file) {
                 setImageUploadError('Please select an image');
-               return;
+                return;
             }
             setImageUploadError(null);
             const storage = getStorage(app);
             const fileName = new Date().getTime() + '-' + file.name;
-            const storageRef = ref(storage , fileName);
-            const uploadTask = uploadBytesResumable(storageRef,file);
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
             uploadTask.on(
                 'state_changed',
                 (snapshot) => {
-                    const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                     setImageUploadProgress(progress.toFixed(0));
                 },
-               (error)=>{
-                setImageUploadError('Image upload failed');
-                setImageUploadProgress(null);
-
+                (error) => {
+                    setImageUploadError('Image upload failed');
+                    setImageUploadProgress(null);
                 },
-                ()=>{
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                         setImageUploadProgress(null);
                         setImageUploadError(null);
-                        setFormData({...formData , image:downloadURL});
+                        setFormData({ ...formData, image: downloadURL });
                     });
                 }
             );
@@ -51,45 +52,102 @@ export default function CreatePost() {
         }
     };
 
- return <div className="p-3 max-w-3xl mx-auto min-h-screen">
-    <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
-    <form className="flex flex-col gap-4">
-        <div className="flex flex-col gap-4 sm:flex-row justify-between">
-            <TextInput type ='text' placeholder='Title' required id='title' className='flex-1'/>
-            <Select>
-                <option value="uncategorized">Select a category</option>
-                <option value="Lifestyle">Lifestyle</option>
-                <option value="Travel">Travel</option>
-                <option value="Food">Food</option>
-                <option value="Fashion & Beauty">Fashion & Beauty</option>
-                <option value="Books & Literature">Books & Literature</option>
-            </Select>
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // Add this to prevent form from reloading
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                setPublishError(data.message);
+                return
+            }
+
+            if (res.ok) {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`);
+            }
+        } catch (error) {
+            setPublishError('Something went wrong');
+        }
+    };
+
+    return (
+        <div className="p-3 max-w-3xl mx-auto min-h-screen">
+            <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-4 sm:flex-row justify-between">
+                    <TextInput
+                        type="text"
+                        placeholder="Title"
+                        required
+                        id="title"
+                        className="flex-1"
+                        onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                    />
+                    <Select
+                        onChange={(e) => {
+                            setFormData({ ...formData, category: e.target.value });
+                        }}
+                        required
+                    >
+                        <option value="uncategorized">Select a category</option>
+                        <option value="Lifestyle">Lifestyle</option>
+                        <option value="Travel">Travel</option>
+                        <option value="Food">Food</option>
+                        <option value="Fashion & Beauty">Fashion & Beauty</option>
+                        <option value="Books & Literature">Books & Literature</option>
+                    </Select>
+                </div>
+                <div className="flex gap-4 items-center justify-between border-4 border-teal-400 border-dotted p-3">
+                    <FileInput type="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
+                    <Button
+                        type="button"
+                        gradientDuoTone="purpleToBlue"
+                        size="sm"
+                        outline
+                        onClick={handleUploadImage}
+                        disabled={imageUploadProgress}
+                    >
+                        {imageUploadProgress ? (
+                            <div className="w-16 h-16">
+                                <CircularProgressbar
+                                    value={imageUploadProgress}
+                                    text={`${imageUploadProgress || 0}%`}
+                                />
+                            </div>
+                        ) : (
+                            'Upload Image'
+                        )}
+                    </Button>
+                </div>
+                {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
+                {formData.image && (
+                    <img
+                        src={formData.image}
+                        alt="upload"
+                        className="w-full h-72 object-cover"
+                    />
+                )}
+                <ReactQuill
+                    theme="snow"
+                    placeholder="Write something..."
+                    className="h-72 mb-12"
+                    required
+                    onChange={(value) => {
+                        setFormData({ ...formData, content: value });
+                    }}
+                />
+                <Button type="submit" gradientDuoTone="purpleToPink">Publish</Button>
+                {publishError && (
+                    <Alert color="failure" className="mt-5">{publishError}</Alert>
+                )}
+            </form>
         </div>
-        <div className="flex gap-4 items-center justify-between border-4 border-teal-400 border-dotted p-3">
-            <FileInput type= 'file' accept='image/*' onChange={(e)=>setFile(e.target.files[0])}/>
-            <Button type= 'button' gradientDuoTone='purpleToBlue' size='sm' outline onClick={handleUploadImage}
-            disabled={imageUploadProgress}>
-                {imageUploadProgress? (
-                    <div className="w-16 h-16">
-                        <CircularProgressbar
-                        value={imageUploadProgress}
-                        text={`${imageUploadProgress || 0}%`}
-                        />
-                    </div>
-                ) : ('Upload Image')}
-            </Button>
-        </div>
-        {imageUploadError && <Alert color ='failure'>{imageUploadError}</Alert>}
-        {formData.image && (
-            <img 
-            src={formData.image}
-            alt='upload'
-            className="w-full h-72 object-cover"
-            />
-        )}
-        <ReactQuill theme="snow" placeholder="Write something..." className="h-72 mb-12" required/>
-        <Button type="submit" gradientDuoTone='purpleToPink'>Publish</Button>
-    </form>
- </div>
-  
+    );
 }
