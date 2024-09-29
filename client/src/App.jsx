2e29574533ad550f44b92647a6824cd919497b2d
anushkapunekar <160164659+anushkapunekar@users.noreply.github.com>
2024-09-29 16:41:02 +0530

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import UpdatePost from "./pages/UpdatePost.jsx";
 import PostPage from "./pages/PostPage.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
+import Search from "./pages/Search.jsx";
 
 export default function App() {
   return (
@@ -24,8 +25,9 @@ export default function App() {
       <Route path = "/about" element = {<About/>}/>
       <Route path = "/sign-in" element = {<Signin />} />
       <Route path = "/sign-up" element = {<Signup />} />
+      <Route path = "/search" element = {<Search />} />
       <Route element={<PrivateRoute/>}>
-     b  <Route path = "/dashboard" element = {<Dashboard/>} />
+      <Route path = "/dashboard" element = {<Dashboard/>} />
       </Route>
       <Route element={<OnlyAdminPrivateRoute/>}>
       <Route path = "/create-post" element = {<CreatePost/>} />
